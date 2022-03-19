4bc85fc1d30754b5cefa1568178b9178b7b9503b
iforgot-my-username <garibay2018paul@gmail.com>
2022-03-20 01:57:44 +0800

    made db seeds

--- back-end/prisma/seed.js
+++ back-end/prisma/seed.js
@@ -2,89 +2,45 @@
 Object.defineProperty(exports, "__esModule", { value: true });
 const client_1 = require("@prisma/client");
 const db = new client_1.PrismaClient();
-Promise.all([
+const persons = [
     db.person.create({
         data: { name: 'Wil Wheaton' }
-    }),
-    db.grudge.create({
-        data: {
-            description: 'failed to show up to the Star Trek convention',
-            rank: 10,
-            person_id: 1,
-        }
     }),
     db.person.create({
         data: { name: 'Billy Sparks' }
     }),
-    db.grudge.create({
-        data: {
-            description: 'Putting dog poop on the handlebars of his bike',
-            rank: 9,
-            person_id: 2,
-        }
-    }),
     db.person.create({
         data: { name: 'Brent Spiner' }
-    }),
-    db.grudge.create({
-        data: {
-            description: "Opened Sheldon's 'Star Trek' character collectible, ruining its integrity",
-            rank: 8,
-            person_id: 3,
-        }
     }),
     db.person.create({
         data: { name: 'Tam Nguyen' }
     }),
-    db.grudge.create({
-        data: {
-            description: "when Sheldon left Texas for California, Tam didn't follow him",
-            rank: 7,
-            person_id: 4,
-        }
-    }),
     db.person.create({
         data: { name: 'Leslie Winkle' }
-    }),
-    db.grudge.create({
-        data: {
-            description: 'shared different scientific views',
-            rank: 6,
-            person_id: 5,
-        }
     }),
     db.person.create({
         data: { name: 'Georgie Cooper' }
     }),
-    db.grudge.create({
-        data: {
-            description: 'peeing in his shampoo bottle',
-            rank: 5,
-            person_id: 6,
-        }
-    }),
     db.person.create({
         data: { name: 'Jim Henson' }
-    }),
-    db.grudge.create({
-        data: {
-            description: 'creating Big Bird because it haunted him in his sleep',
-            rank: 4,
-            person_id: 7,
-        }
     }),
     db.person.create({
         data: { name: 'Joel Schumacher' }
     }),
-    db.grudge.create({
-        data: {
-            description: 'worked on Batman Forever and Batman & Robin',
-            rank: 3,
-            person_id: 8,
-        }
-    }),
     db.person.create({
         data: { name: 'Penny' }
+    }),
+    db.person.create({
+        data: { name: 'The Cafeteria Ladies' }
+    }),
+];
+const grudges = [
+    db.grudge.create({
+        data: {
+            description: 'refused to tell him what was in the chili',
+            rank: 1,
+            person_id: 10,
+        }
     }),
     db.grudge.create({
         data: {
@@ -93,16 +49,67 @@ Promise.all([
             person_id: 9,
         }
     }),
-    db.person.create({
-        data: { name: 'The Cafeteria Ladies' }
+    db.grudge.create({
+        data: {
+            description: 'worked on Batman Forever and Batman & Robin',
+            rank: 3,
+            person_id: 8,
+        }
     }),
     db.grudge.create({
         data: {
-            description: 'refused to tell him what was in the chili',
-            rank: 1,
-            person_id: 10,
+            description: 'creating Big Bird because it haunted him in his sleep',
+            rank: 4,
+            person_id: 7,
+        }
+    }), db.grudge.create({
+        data: {
+            description: 'peeing in his shampoo bottle',
+            rank: 5,
+            person_id: 6,
         }
     }),
-])
-    .then(() => console.log('All dummy succes'))
-    .catch(() => console.error('Something went wrong with seeding'));
+    db.grudge.create({
+        data: {
+            description: 'shared different scientific views',
+            rank: 6,
+            person_id: 5,
+        }
+    }),
+    db.grudge.create({
+        data: {
+            description: "when Sheldon left Texas for California, Tam didn't follow him",
+            rank: 7,
+            person_id: 4,
+        }
+    }),
+    db.grudge.create({
+        data: {
+            description: "Opened Sheldon's 'Star Trek' character collectible, ruining its integrity",
+            rank: 8,
+            person_id: 3,
+        }
+    }),
+    db.grudge.create({
+        data: {
+            description: 'Putting dog poop on the handlebars of his bike',
+            rank: 9,
+            person_id: 2,
+        }
+    }),
+    db.grudge.create({
+        data: {
+            description: 'failed to show up to the Star Trek convention',
+            rank: 10,
+            person_id: 1,
+        }
+    }),
+];
+Promise.all(persons)
+    .then(() => {
+    console.log('All dummy person succes');
+    Promise.all(grudges)
+        .then(() => console.log("Success finally!!!"))
+        .catch((e) => console.error('Something went wrong with seeding', e));
+})
+    .catch((e) => console.error('Something went wrong with seeding', e));
--- back-end/prisma/seed.ts
+++ back-end/prisma/seed.ts
@@ -2,132 +2,125 @@ import { PrismaClient, Person, Grudge } from '@prisma/client'
 
 const db = new PrismaClient()
 
-Promise.all([
+const persons = [
     db.person.create({
         data: { name: 'Wil Wheaton' }
     }),
-
-    db.grudge.create({
-        data: { 
-            description: 'failed to show up to the Star Trek convention',
-            rank: 10,
-            person_id: 1,
-            
-        }
-    }),
-    
     db.person.create({
-        data: { name: 'Billy Sparks'}
+        data: { name: 'Billy Sparks' }
     }),
-    
-    db.grudge.create({
-        data: { 
-            description: 'Putting dog poop on the handlebars of his bike',
-            rank: 9,
-            person_id: 2,
-            
-        }
-    }),
-    
     db.person.create({
-        data: { name: 'Brent Spiner'}
+        data: { name: 'Brent Spiner' }
     }),
-
-    db.grudge.create({
-        data: { 
-            description: "Opened Sheldon's 'Star Trek' character collectible, ruining its integrity",
-            rank: 8,
-            person_id: 3,
-            
-        }
-    }),
-  
     db.person.create({
-        data: { name: 'Tam Nguyen'}
+        data: { name: 'Tam Nguyen' }
     }),
-
-    db.grudge.create({
-        data: { 
-            description: "when Sheldon left Texas for California, Tam didn't follow him",
-            rank: 7,
-            person_id: 4,
-            
-        }
-    }),
-    
     db.person.create({
-        data: { name: 'Leslie Winkle'}
+        data: { name: 'Leslie Winkle' }
     }),
-
-    db.grudge.create({
-        data: { 
-            description: 'shared different scientific views',
-            rank: 6,
-            person_id: 5,
-        }
-    }),
-    
     db.person.create({
         data: { name: 'Georgie Cooper' }
     }),
-
-    db.grudge.create({
-        data: { 
-            description: 'peeing in his shampoo bottle',
-            rank: 5,
-            person_id: 6,
-        }
-    }),
-    
     db.person.create({
         data: { name: 'Jim Henson' }
     }),
-
-    db.grudge.create({
-        data: { 
-            description: 'creating Big Bird because it haunted him in his sleep',
-            rank: 4,
-            person_id: 7,
-        }
-    }),
-
     db.person.create({
         data: { name: 'Joel Schumacher' }
     }),
+    db.person.create({
+        data: { name: 'Penny' }
+    }),
 
+    db.person.create({
+        data: { name: 'The Cafeteria Ladies' }
+    }),
+]
+
+const grudges = [
     db.grudge.create({
-        data: { 
-            description: 'worked on Batman Forever and Batman & Robin',
-            rank: 3,
-            person_id: 8,
+        data: {
+            description: 'refused to tell him what was in the chili',
+            rank: 1,
+            person_id: 10,
         }
     }),
-   
-    db.person.create({
-        data: { name: 'Penny'}
-    }),
-
     db.grudge.create({
-        data: { 
+        data: {
             description: "was informing Amy of Sheldon's list of mortal enemies",
             rank: 2,
             person_id: 9,
         }
     }),
 
-    db.person.create({
-        data: { name: 'The Cafeteria Ladies'}
-    }),
-
     db.grudge.create({
-        data: { 
-            description: 'refused to tell him what was in the chili',
-            rank: 1,
-            person_id: 10,
+        data: {
+            description: 'worked on Batman Forever and Batman & Robin',
+            rank: 3,
+            person_id: 8,
+        }
+    }),
+    db.grudge.create({
+        data: {
+            description: 'creating Big Bird because it haunted him in his sleep',
+            rank: 4,
+            person_id: 7,
+        }
+    }), db.grudge.create({
+        data: {
+            description: 'peeing in his shampoo bottle',
+            rank: 5,
+            person_id: 6,
+        }
+    }),
+    db.grudge.create({
+        data: {
+            description: 'shared different scientific views',
+            rank: 6,
+            person_id: 5,
+        }
+    }),
+    db.grudge.create({
+        data: {
+            description: "when Sheldon left Texas for California, Tam didn't follow him",
+            rank: 7,
+            person_id: 4,
+
+        }
+    }),
+    db.grudge.create({
+        data: {
+            description: "Opened Sheldon's 'Star Trek' character collectible, ruining its integrity",
+            rank: 8,
+            person_id: 3,
+
+        }
+    }),
+    db.grudge.create({
+        data: {
+            description: 'Putting dog poop on the handlebars of his bike',
+            rank: 9,
+            person_id: 2,
+
+        }
+    }),
+    db.grudge.create({
+        data: {
+            description: 'failed to show up to the Star Trek convention',
+            rank: 10,
+            person_id: 1,
+
         }
     }),
 
-])
-    .then(() => console.log('All dummy succes'))
-    .catch(() => console.error('Something went wrong with seeding'))
+]
+
+Promise.all(persons)
+    .then(() => {
+        console.log('All dummy person succes')
+
+        Promise.all(grudges)
+            .then(() => console.log("Success finally!!!"))
+            .catch((e) => console.error('Something went wrong with seeding', e))
+    })
+    .catch((e) => console.error('Something went wrong with seeding', e))
 
