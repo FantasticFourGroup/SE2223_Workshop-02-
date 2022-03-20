c3880973d5761d3bdf541ea87a5c173c051aded8
iforgot-my-username <garibay2018paul@gmail.com>
2022-03-20 16:19:13 +0800

    test

--- back-end/index.js
+++ back-end/index.js
@@ -9,10 +9,10 @@ const api_1 = __importDefault(require("./routes/api"));
 const app = (0, express_1.default)();
 const PORT = process.env.PORT || 3000;
 app.use((0, cors_1.default)())
-    .use('/api', api_1.default)
     .get('/', (req, res) => {
     res.send('<h1>Well done!<h1>');
-});
+})
+    .use('/api', api_1.default);
 app.listen(PORT, () => {
     console.log(`Open http://localhost:${PORT}`);
 });
